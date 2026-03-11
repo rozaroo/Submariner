77c2546f1d13d61b4f3a870a81a71f421146cd07
roberto <roramos@uade.edu.ar>
2026-03-11 13:34:27 -0300

    Codigos escritos

--- Assets/Scripts/Roberto/CameraFollow.cs
+++ Assets/Scripts/Roberto/CameraFollow.cs
@@ -1,16 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform target;
+    private float movSmoothing;
+    private float rotSmoothing;
     void Start()
     {
-        
+        movSmoothing = 0.04;
+        rotSmoothing = 0.02;
+        transform.position = target.position;
+        transform.rotation = target.rotation;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, target.position, movSmoothing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotSmoothing);
     }
 }
--- Assets/Scripts/Roberto/SubController.cs
+++ Assets/Scripts/Roberto/SubController.cs
@@ -1,16 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SubController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created<<>>
+    public float speedChangeAmount;
+    public float maxForwardSpeed;
+    public float maxBackwardSpeed;
+    public float minSpeed;
+    public float turnSpeed;
+    public float curSpeed;
+    public float riseSpeed;
+    public float stabilizationSmoothing;
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    { 
+        Move();
+        Turn();
+        Rise();
+        Stabilize();
+    }
+    void Move() 
     {
-        
+        if (Input.GetKey(KeyCode.W)) curSpeed += speedChangeAmount;
+        else if (Input.GetKey(KeyCode.S)) curSpeed -= speedChangeAmount;
+        else if (Mathf.Abs(curSpeed) <= minSpeed) curSpeed = 0;
+        curSpeed = Mathf.Clamp(curSpeed, -maxBackwardSpeed, maxForwardSpeed);
+        rb.AddForce(transform.forward * curSpeed);
+    }
+    void Turn() 
+    {
+        if (Input.GetKey(KeyCode.D)) rb.AddTorque(transform.up * turnSpeed);
+        else if (Input.GetKey(KeyCode.A)) rb.AddTorque(transform.up * -turnSpeed);
+    }
+    void Rise() 
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) rb.AddForce(transform.up * riseSpeed);
+        else if (Input.GetKey(KeyCode.LeftControl)) rb.AddForce(transform.up * -riseSpeed);
+    }
+    void Stabilize() 
+    {
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, Quaternion.Euler(new Vector3(0, rb.rotation.eulerAngles.y, 0)), stabilizationSmoothing));
     }
 }
