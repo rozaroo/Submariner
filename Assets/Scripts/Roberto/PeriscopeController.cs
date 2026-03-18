1fb3b794cf5b8a15bf13e4c932a6ef0cc80e4f8a
roberto <roramos@uade.edu.ar>
2026-03-18 15:16:54 -0300

    Scripts del Periscopio

--- Assets/Scripts/Roberto/PeriscopeController.cs
+++ Assets/Scripts/Roberto/PeriscopeController.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PeriscopeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created<>
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 50f;
+    public Transform periscopeHead;
+    public InputActionReference moveAction;
+    bool isActive = false;
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable() 
     {
-        
+        moveAction.action.Enable();
+    }
+    void OnDisable()
+    {
+        moveAction.action.Disable();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created<>
+    public void ActivatePeriscope(bool state) 
+    {
+        isActive = state;
+    }
+    void Update() 
+    {
+        if (!isActive) return;
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        float turn = input.x; //A / D
+        periscopeHead.Rotate(Vector3.up * turn * rotationSpeed * Time.deltaTime);
     }
 }
