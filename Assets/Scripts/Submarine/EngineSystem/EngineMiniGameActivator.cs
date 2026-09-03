90bb1b6e602df06d7477576db482ba37c02116f3
rozaroo <roramos@uade.edu.ar>
2026-09-03 00:17:26 -0300

    Cambio de camara
    Aun no está terminado

--- Assets/Scripts/Submarine/EngineSystem/EngineMiniGameActivator.cs
+++ Assets/Scripts/Submarine/EngineSystem/EngineMiniGameActivator.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
 
-public class EngineMiniGameActivator : MonoBehaviour
+public class EngineMiniGameActivator : MonoBehaviour, IPossessable, IInteractable
 {
+    [Header("References")]
     [SerializeField] private EngineMiniGame engineMiniGame;
+
+    [Header("Possession Config")]
+    [SerializeField] private Transform cameraAnchor;
+    [SerializeField] private Transform directionAnchor;
+    [SerializeField] private float transitionDuration = 0.1f;
+    [SerializeField] private CursorLockMode cursorLockMode;
+    [SerializeField] private bool showMouseCursor;
+
+    [Header("Actions Maps Settings")]
+    [SerializeField] private string stationMapName;
+
     private bool _playerInside;
+
+    public string MapName => stationMapName;
+    public Transform CameraAnchor => cameraAnchor;
+    public Transform DirectionAnchor => directionAnchor;
+    public float TransitionDuration => transitionDuration;
+    public CursorLockMode CursorLockMode => cursorLockMode;
+    public bool IsMouseVisible => showMouseCursor;
 
     public void Activate(PlayerCharacter player)
     {
@@ -16,14 +35,28 @@ public class EngineMiniGameActivator : MonoBehaviour
         engineMiniGame.StartMinigame();
     }
 
+    public void Interact(PlayerCharacter player)
+    {
+        player.OnPossessionState(this);
+    }
+    public void Possess(PlayerCharacter player)
+    {
+        Activate(player);
+    }
+
+    public void UnPossess()
+    {
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<PlayerCharacter>(out _))  _playerInside = true;
+        if (other.TryGetComponent<PlayerCharacter>(out _))
+            _playerInside = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
-       if (other.TryGetComponent<PlayerCharacter>(out _)) _playerInside = false;
+        if (other.TryGetComponent<PlayerCharacter>(out _))
+            _playerInside = false;
     }
 }
