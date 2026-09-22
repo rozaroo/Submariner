071b23f232950d37f9088538d6734694c8610a18
Edohhhh <elric9959@gmail.com>
2026-09-22 15:14:39 -0300

    FixV1

--- Assets/Scripts/Submarine/LightObject.cs
+++ Assets/Scripts/Submarine/LightObject.cs
@@ -8,29 +8,87 @@ public class LightObject : MonoBehaviour
     [SerializeField] private Color lightColor = Color.white;
     [Range(0f, 10f)]
     [SerializeField] private float intensity = 0.01f;
+
+    [Header("Material Settings")]
+    [Tooltip("La intensidad de emisión base del material. Pon aquí el mismo valor que tienes en tu shader (ej. 1).")]
+    [SerializeField] private float materialEmissionIntensity = 1f;
+
     private MaterialPropertyBlock _lightMaterialPropertyBlock;
-    public Color LightColor { get; }
+    private bool _isToggledOn = true;
+
+    
+    private Color _initialLightColor;
+    private float _initialLightIntensity;
+    private Color _baseMatLightColor;
+    private Color _baseMatEmissionColor;
+
+    public Color LightColor => lightColor;
     public float Intensity => intensity;
 
     public void Initialize()
     {
         _lightMaterialPropertyBlock = new MaterialPropertyBlock();
+
+        if (lightComponent != null)
+        {
+            _isToggledOn = lightComponent.enabled;
+
+          
+            lightColor = lightComponent.color;
+            intensity = lightComponent.intensity;
+
+            _initialLightColor = lightComponent.color;
+            _initialLightIntensity = lightComponent.intensity;
+        }
+
+        if (targetRenderer != null)
+        {
+            
+            _baseMatLightColor = targetRenderer.sharedMaterial.GetColor("_LightColor");
+            _baseMatEmissionColor = targetRenderer.sharedMaterial.GetColor("_EmissionColor");
+        }
+
+        ApplySettings();
     }
-    
+
     private void ApplySettings()
     {
         if (lightComponent != null)
         {
             lightComponent.color = lightColor;
-            lightComponent.intensity = intensity;
+            lightComponent.intensity = _isToggledOn ? intensity : 0f;
         }
-        
+
         if (targetRenderer != null)
         {
             targetRenderer.GetPropertyBlock(_lightMaterialPropertyBlock);
-            _lightMaterialPropertyBlock.SetColor("_LightColor", lightColor);
-            _lightMaterialPropertyBlock.SetColor("_EmissionColor", lightColor * intensity);
-            _lightMaterialPropertyBlock.SetFloat("_EmissionIntensity", intensity * 100000f);
+
+            
+            if (lightColor == _initialLightColor)
+            {
+                _lightMaterialPropertyBlock.SetColor("_LightColor", _baseMatLightColor);
+                _lightMaterialPropertyBlock.SetColor("_EmissionColor", _baseMatEmissionColor);
+            }
+            else
+            {
+               
+                _lightMaterialPropertyBlock.SetColor("_LightColor", lightColor);
+                _lightMaterialPropertyBlock.SetColor("_EmissionColor", lightColor);
+            }
+
+            
+            float intensityRatio = 1f;
+            if (_initialLightIntensity > 0.001f)
+            {
+               
+                intensityRatio = intensity / _initialLightIntensity;
+            }
+
+            
+            float currentEmission = _isToggledOn ? (materialEmissionIntensity * intensityRatio) : 0f;
+
+            _lightMaterialPropertyBlock.SetFloat("_EmissionIntensity", currentEmission);
+
             targetRenderer.SetPropertyBlock(_lightMaterialPropertyBlock);
         }
     }
@@ -49,9 +107,8 @@ public class LightObject : MonoBehaviour
 
     public void Toggle(bool lightEnabled)
     {
-        lightComponent.enabled = lightEnabled;
-        lightColor = lightEnabled ? lightColor : Color.black;
-        lightComponent.intensity = lightEnabled ? intensity : 0f;
+        _isToggledOn = lightEnabled;
+        if (lightComponent != null) lightComponent.enabled = lightEnabled;
         ApplySettings();
     }
 }
